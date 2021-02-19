474d11bcd0f9b1b45d75767a2aab2593e7f000cf
Jason Davis <jasondavis3@gmail.com>
2021-02-18 18:10:52 -0800

    updated file structure

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,41 @@ import User from "../backend/models/UserModel"
 import mongoose from "mongoose";
 
 const router = express.Router();
-/* GET home page. */
+
 router
+    /* GET home page. */
 	.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+        res.render('index', 
+            { 
+                pageMainClass: 'pgMainHome',
+                title: 'COP Final Project',
+                msg: 'fun time',
+                group: 'The whole class'
+            });
 	})
+    /* POST user page. */
 	.post('/user', (req, res, next) =>{
 		/*console.log(req.body);*/
 		/*console.log(req.headers);*/
-    const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        salary: req.body.salary
-    });
-    /*res.status(200).json({
+        const user = new User({
+            _id: mongoose.Types.ObjectId(),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            salary: req.body.salary
+        });
+        /*res.status(200).json({
             res: [req.body.firstName]
         });*/
-    user.save()
-    .then(result => {
-        res.status(200).json({
-            docs:[user]
+        user.save()
+        .then(result => {
+            res.status(200).json({
+                docs:[user]
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-    })
-    .catch(err => {
-        console.log(err);
     });
-	});
 
 module.exports = router;
